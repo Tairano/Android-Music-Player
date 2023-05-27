1e8161ec8f5fa795fdfba929db64e1e2618430c9
Tairano <2023684800@qq.com>
2023-05-27 16:31:12 +0800

    feat: 新增初始界面底部栏，播放栏，搜索栏（未实现逻辑功能） feat: 新增我的，关注（未写），社区（未写）页面 feat: 新增播放页面（未写） feat: 新增歌单列表页面（未写）

--- app/src/main/java/com/example/androidmusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/androidmusicplayer/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.androidmusicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+val fragments = arrayOf(
+    "我的" to Mine(),
+    "关注" to Concern(),
+    "社区" to Community()
+)
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tabLayout: TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initPage()
+    }
+
+    private fun initPage(){
+        viewPager2 = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+        val adapter = MyViewPager2Adapter(this)
+        viewPager2.adapter = adapter
+        TabLayoutMediator(tabLayout, viewPager2) {tab, position ->
+            tab.text = fragments[position].first
+        }.attach()
+    }
+
+    private inner class MyViewPager2Adapter(fm : AppCompatActivity) : FragmentStateAdapter(fm) {
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment = fragments[position].second
     }
 }
---
+++ app/src/main/java/com/example/androidmusicplayer/PlayListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidmusicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_list)
+    }
+}
