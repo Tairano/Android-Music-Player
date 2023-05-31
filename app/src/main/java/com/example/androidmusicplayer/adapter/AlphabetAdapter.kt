73e4aecc5a4120fcfb3a9920d8d566fcc6829cbc
Tairano <2023684800@qq.com>
2023-05-31 18:33:42 +0800

    feat(AlphabetAdapter): 实现本地歌曲的歌曲列表的卡片的弹窗菜单功能

--- app/src/main/java/com/example/androidmusicplayer/adapter/AlphabetAdapter.kt
+++ app/src/main/java/com/example/androidmusicplayer/adapter/AlphabetAdapter.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmusicplayer.R
@@ -18,6 +21,7 @@ class AlphabetAdapter (private val dataSet: ArrayList<Play>, val context: Fragme
         val index : TextView = view.findViewById(R.id.index)
         val name : TextView = view.findViewById(R.id.name)
         val author : TextView = view.findViewById(R.id.author)
+        val more : Button = view.findViewById(R.id.more)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,12 +38,34 @@ class AlphabetAdapter (private val dataSet: ArrayList<Play>, val context: Fragme
         return viewHolder
     }
 
+    private fun popUpMenu(v: View?){
+        val popupMenu = PopupMenu(context.activity,v)
+        popupMenu.inflate(R.menu.play_more_menu)
+        popupMenu.setOnMenuItemClickListener{ item ->
+            when( item.itemId ){
+                R.id.add_in_next ->{
+                    Toast.makeText(context.activity, "此功能暂未实现，敬请期待。", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.add_in_list ->{
+                    Toast.makeText(context.activity, "此功能暂未实现，敬请期待。", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show()
+    }
+
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val play = dataSet[position]
         holder.index.text = (position + 1).toString()
         holder.author.text = "-" + play.author
         holder.name.text = play.name
+        holder.more.setOnClickListener {
+            popUpMenu(holder.more)
+        }
     }
 
     override fun getItemCount() = dataSet.size
