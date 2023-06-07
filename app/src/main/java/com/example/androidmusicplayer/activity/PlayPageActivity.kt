f6cd89ffe38bc9c0bcc9ecbdff3ef36f4a6e8630
Tairano <2023684800@qq.com>
2023-06-07 10:02:20 +0800

    feat: 实现切歌时歌曲名称和作者的变化

--- app/src/main/java/com/example/androidmusicplayer/activity/PlayPageActivity.kt
+++ app/src/main/java/com/example/androidmusicplayer/activity/PlayPageActivity.kt
@@ -1,38 +1,88 @@
 package com.example.androidmusicplayer.activity
 
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import com.example.androidmusicplayer.R
 import com.example.androidmusicplayer.struct.Play
+import com.example.androidmusicplayer.struct.PlayService
 
 class PlayPageActivity : AppCompatActivity() {
+    private var playing = true
     private lateinit var play : Play
+    private lateinit var binder : PlayService.PlayBinder
+    private lateinit var songName: TextView
+    private lateinit var author: TextView
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            binder = service as PlayService.PlayBinder
+            binder.play(play)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            TODO("Not yet implemented")
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_page)
         val intent = intent
         play = intent.getSerializableExtra("play") as Play
-        initMediaPlayer()
         initPage()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     private fun initPage(){
-        val songName: TextView = findViewById(R.id.song_name)
-        val author: TextView = findViewById(R.id.author)
         val goBack : Button = findViewById(R.id.go_back)
+        songName = findViewById(R.id.song_name)
+        author = findViewById(R.id.author)
+
         songName.text = play.name
         author.text = play.author
         goBack.setOnClickListener {
             this.finish()
         }
+        val intents = Intent(this, PlayService::class.java)
+        bindService(intents, connection, Context.BIND_AUTO_CREATE)
+
+        val preSong : Button = findViewById(R.id.pre_song)
+        val nextSong : Button = findViewById(R.id.next_song)
+        val playButton : Button = findViewById(R.id.play)
+        preSong.setOnClickListener {
+            binder.pre()
+            resetTitle()
+        }
+        playButton.setOnClickListener {
+            binder.pause()
+            if(playing){
+                playButton.setBackgroundResource(R.drawable.stop)
+                playing = false
+            }
+            else {
+                playButton.setBackgroundResource(R.drawable.play)
+                playing = true
+            }
+        }
+        nextSong.setOnClickListener {
+            binder.next()
+            resetTitle()
+        }
     }
 
-    private fun initMediaPlayer() {
-        val assetManager = assets
-        val fd = assetManager.openFd(play.path)
+    private fun resetTitle(){
+        songName.text = binder.getName()
+        author.text = binder.getAuthor()
     }
 }
--- app/src/main/java/com/example/androidmusicplayer/struct/PlayService.kt
+++ app/src/main/java/com/example/androidmusicplayer/struct/PlayService.kt
@@ -1,35 +1,113 @@
 package com.example.androidmusicplayer.struct
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
 import android.graphics.BitmapFactory
 import android.media.MediaPlayer
-import android.os.Build
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import com.example.androidmusicplayer.MainActivity
 import com.example.androidmusicplayer.R
 
-class PlayService : Service() {
-    private lateinit var mediaPlayer : MediaPlayer
+open class PlayService : Service() {
+    private val binder = PlayBinder()
+    private lateinit var player : MediaPlayer
+    private lateinit var assetManager : AssetManager
+    private lateinit var fd : AssetFileDescriptor
+    private var path = ""
+    private val playList = ArrayList<Play>()
+    private var point = 0
+
+    fun preSong(){
+        point --
+        if(point < 0)
+            point = playList.size - 1
+        playInList()
+        addInRecentPlayed()
+    }
+
+    fun nextSong(){
+        point ++
+        if(point > playList.size - 1)
+            point = 0
+        playInList()
+        addInRecentPlayed()
+    }
+
+    fun pauseOrPlay(){
+        if(player.isPlaying)
+            player.pause()
+        else player.start()
+    }
+
+    fun addSong(play: Play){
+        point = if(!playList.contains(play)){
+            playList.add(play)
+            playList.size -1
+        } else {
+            playList.indexOf(play)
+        }
+        playInList()
+        addInRecentPlayed()
+    }
+
+    fun addInRecentPlayed(){
+        Log.d("music","good")
+    }
+
+    fun playInList(){
+        if(path != playList[point].path){
+            path = playList[point].path
+            player.stop()
+            player.reset()
+            setSource(path)
+        }
+        player.start()
+        val intent = Intent("PlayPageActivity")
+        intent.putExtra("titleName",playList[point].name)
+        intent.putExtra("titleAuthor",playList[point].author)
+    }
+
+    fun setSource(filePath: String){
+        fd = assetManager.openFd(filePath)
+        player.setDataSource(fd.fileDescriptor,fd.startOffset,fd.length)
+        player.prepare()
+    }
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return binder
     }
 
     override fun onCreate() {
-        Log.d("play-service", "created..")
         super.onCreate()
+        assetManager = assets
+        val service = Intent(this,PlayService::class.java)
+        startService(service)
+    }
+
+    override fun onDestroy() {
+        player.stop()
+        player.release()
+        super.onDestroy()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("play-service", "onStartCommand..")
+        startForeground()
+        player = MediaPlayer()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForeground(){
         // 创建前台
         val channelId = "AndroidMusicPlayer.foreground"
         val channelName = "Android Music Player"
@@ -38,6 +116,23 @@ class PlayService : Service() {
         manager.createNotificationChannel(channel)
         val intents = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, intents, PendingIntent.FLAG_IMMUTABLE)
+        val view = RemoteViews(this.packageName, R.layout.foreground)
+
+        val preButton = Intent(this, MyBroadcastReceiver::class.java)
+        preButton.action = PRE_ACTION
+        val pendingPreButton = PendingIntent.getBroadcast(this, 0, preButton, PendingIntent.FLAG_IMMUTABLE)
+
+        val playButton = Intent(this, MyBroadcastReceiver::class.java)
+        playButton.action = PLAY_ACTION
+        val pendingPlayButton = PendingIntent.getBroadcast(this, 0, playButton, PendingIntent.FLAG_IMMUTABLE)
+
+        val nextButton = Intent(this, MyBroadcastReceiver::class.java)
+        nextButton.action = NEXT_ACTION
+        val pendingNextButton = PendingIntent.getBroadcast(this, 0, nextButton, PendingIntent.FLAG_IMMUTABLE)
+
+        view.setOnClickPendingIntent(R.id.pre_song,pendingPreButton)
+        view.setOnClickPendingIntent(R.id.play,pendingPlayButton)
+        view.setOnClickPendingIntent(R.id.next_song,pendingNextButton)
 
         val notification = NotificationCompat.Builder(this, channelId)
             .setContentTitle(channelName)
@@ -46,19 +141,52 @@ class PlayService : Service() {
             .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.logo))
             .setWhen(System.currentTimeMillis())
             .setContentIntent(pendingIntent)
+            .setContent(view)
             .build()
         startForeground(1, notification)
-        mediaPlayer = MediaPlayer()
-        //        mediaPlayer.setDataSource(fd.fileDescriptor,fd.startOffset,fd.length)
-//        mediaPlayer.prepare()
-//        mediaPlayer.start()
-        return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onDestroy() {
-        mediaPlayer.stop()
-        mediaPlayer.release()
-        super.onDestroy()
-        Log.d("play-service", "destory executed..")
+    inner class PlayBinder : Binder() {
+        fun pre(){
+            preSong()
+        }
+
+        fun next(){
+            nextSong()
+        }
+
+        fun pause(){
+            pauseOrPlay()
+        }
+
+        fun play(play: Play){
+            addSong(play)
+        }
+
+        fun getName(): String = playList[point].name
+
+        fun getAuthor(): String = playList[point].author
+
+    }
+
+    inner class MyBroadcastReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (action == PRE_ACTION) {
+                preSong()
+            }
+            else if (action == PLAY_ACTION) {
+                pauseOrPlay()
+            }
+            else if (action == NEXT_ACTION) {
+                nextSong()
+            }
+        }
+    }
+
+    companion object {
+        const val PRE_ACTION = "com.example.PRE_ACTION"
+        const val PLAY_ACTION = "com.example.PLAY_ACTION"
+        const val NEXT_ACTION = "com.example.NEXT_ACTION"
     }
 }
